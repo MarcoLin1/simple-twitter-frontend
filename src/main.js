6a6a48f259592e87f9a76b3895e466c4735a854f
tinahung126 <tina126hung@gmail.com>
2021-07-06 14:04:04 +0800

    delete fintawesome setting in package.json

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import App from './App.vue'
 import router from './router'
 import './assets/scss/reset.scss'
 
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
-
 Vue.config.productionTip = false
 
 new Vue({
