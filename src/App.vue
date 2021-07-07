191e4469c2657fb0cf48083da1f48eeccd4a25bb
Marco Lin <pohanlin0605@gmail.com>
2021-07-07 22:18:12 +0800

    fix merge conflict

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <template>
   <div id="app">
     <router-view />
-    <TopUsersList />
-    <div class="m-4">
-      <Post />
-    </div>
   </div>
 </template>
 
 <script>
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import TopUsersList from './components/TopUsersList.vue'
-import Post from './components/Post.vue'
 
 export default {
   components: {
-    TopUsersList,
-    Post
+
   }
 }
 </script>
