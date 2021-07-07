42cf6b08ad446c02d419c0e9f4a614582454d0f9
Marco Lin <pohanlin0605@gmail.com>
2021-07-08 00:46:03 +0800

    feat: add UserFollowers view

---
+++ src/views/UserFollowers.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="user__followers__container">
+    <div class="user__followers__side__navbar__wrapper">
+      <SideNavbar />
+    </div>
+    <div class="user__followers__main__wrapper">
+      <TopNavbar
+        :user="user"
+        :current-page="currentPage"
+      />
+      <UsersList />
+    </div>
+    <div class="user__followers__top__users__list__wrapper">
+      <TopUsersList />
+    </div>
+  </div>
+</template>
+
+<script>
+import SideNavbar from './../components/SideNavbar.vue'
+import TopNavbar from './../components/TopNavbar.vue'
+import TopUsersList from './../components/TopUsersList.vue'
+import UsersList from './../components/UsersList.vue'
+const dummyData = {
+  users:
+    {
+      id: 1,
+      name: 'root',
+      email: 'root@example.com',
+      password: '$2a$10$K2x6pQHkzPEKzw86x8Tc0.bfW7QVdA2Ls4AXBFkFu7xHG3UgA4Mli',
+      isAdmin: true,
+      image: 'https://i.pravatar.cc/300',
+      createdAt: '2021-07-05T09:58:39.000Z',
+      updatedAt: '2021-07-05T10:31:19.000Z',
+      Followers: [],
+      FollowerCount: 0,
+      TweetCount: 15,
+      isFollowed: false
+    },
+  isAuthenticated: true
+}
+export default {
+  components: {
+    SideNavbar,
+    TopNavbar,
+    TopUsersList,
+    UsersList
+  },
+  data () {
+    return {
+      user: {
+        id: -1,
+        name: '',
+        email: '',
+        image: '',
+        isAdmin: '',
+        tweetLength: '',
+        isFollowed: false
+      },
+      currentPage: 'UserFollowers'
+    }
+  },
+  created () {
+    this.fetchUser()
+  },
+  methods: {
+    fetchUser () {
+      this.user = {
+        ...this.user,
+        ...dummyData.users
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/main.scss';
+  .user__followers__container {
+    display: flex;
+    justify-content: space-evenly;
+  }
+  .user__followers__main__wrapper {
+    width: 600px;
+    border: 1px solid $light-gray;
+  }
+</style>
