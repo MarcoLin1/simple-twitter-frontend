62f1000679515e123c13faaeef41b59aae2eb350
tinahung126 <tina126hung@gmail.com>
2021-07-07 15:20:38 +0800

    feat:update UserProfile UI layout

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="app">
     <router-view />
+    <div class="m-4">
+      <UserProfile />
+    </div>
+
     <TopUsersList />
     <SideNavBar />
-    <div class="m-4">
-      <Post />
-    </div>
   </div>
 </template>
 
 <script>
+
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import TopUsersList from './components/TopUsersList.vue'
-import Post from './components/Post.vue'
+import UserProfile from './components/UserProfile.vue'
 import SideNavBar from './components/SideNavbar.vue'
 export default {
   components: {
     TopUsersList,
-    Post,
+    UserProfile,
     SideNavBar
 
   }
