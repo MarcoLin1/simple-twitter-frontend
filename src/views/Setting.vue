89303e8f2ff4a460e49485f7e58a51521750782b
Marco Lin <pohanlin0605@gmail.com>
2021-07-07 23:48:59 +0800

    add Setting view

---
+++ src/views/Setting.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="main-container">
+    <SideNavbar />
+    <div class="setting-form-wrapper">
+      <AccountDetailForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import SideNavbar from './../components/SideNavbar.vue'
+import AccountDetailForm from './../components/AccountDetailForm.vue'
+export default {
+  components: {
+    SideNavbar: SideNavbar,
+    AccountDetailForm: AccountDetailForm
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .main-container {
+    display: flex;
+    height: 100%;
+    width: auto;
+  }
+  .setting-form-wrapper {
+    width: 100%;
+  }
+
+</style>
