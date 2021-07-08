30bef680db8d86f2b5cb865fd2067375a61a1fde
Marco Lin <pohanlin0605@gmail.com>
2021-07-08 22:48:28 +0800

    fix conflict

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
   <div id="app">
     <router-view />
-    <div class="m-4">
-      <UserProfile />
-      <Post />
-    </div>
-
-    <TopUsersList />
-    <SideNavBar />
+    <Post />
   </div>
 </template>
 
@@ -15,18 +9,10 @@
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import TopUsersList from './components/TopUsersList.vue'
-import UserProfile from './components/UserProfile.vue'
-import SideNavBar from './components/SideNavbar.vue'
 import Post from './components/Post.vue'
-
 export default {
   components: {
-    TopUsersList,
-    UserProfile,
-    SideNavBar,
     Post
-
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,24 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 const routes = [
   {
-    path: '/user/1/followings',
-    name: 'user-followings',
-    component: () => import('./../views/UserFollowings.vue')
+    path: '/login',
+    name: 'login',
+    component: () => import('./../components/LoginForm.vue')
   },
   {
-    path: '/user/1/followers',
-    name: 'user-followers',
-    component: () => import('./../views/UserFollowers.vue')
+    path: '/user/1/tweets',
+    name: 'user-tweets',
+    component: () => import('./../views/UserTweets.vue')
+  },
+  {
+    path: '/user/1/likes',
+    name: 'user-likes',
+    component: () => import('./../views/UserLikes.vue')
+  },
+  {
+    path: '/user/1/replies',
+    name: 'user-replies',
+    component: () => import('./../views/UserReplies.vue')
   },
   {
     path: '/',
