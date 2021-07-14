0cea1267e36406cd29c67d12928859071bd3ef4c
Marco Lin <pohanlin0605@gmail.com>
2021-07-14 23:32:22 +0800

    feat: update user personal page

--- src/views/UserLikes.vue
+++ src/views/UserLikes.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="user__likes__container">
     <div class="user__likes__main__wrapper">
-      <UserProfile :get-current-user="currentUser" />
+      <UserProfile
+        :get-current-user="currentUser"
+        :initial-user="initialUser"
+        :initial-following="initialFollowing"
+        :user-id="userId"
+      />
       <UserPostItem :user-id="userId" />
       <UserPost
         v-for="post in userLikes"
@@ -19,6 +24,7 @@ import UserPost from './../components/UserPost.vue'
 import UserPostItem from './../components/UserPostItem.vue'
 import { Toast } from '../utils/helper'
 import userAPI from './../apis/users'
+import { mapState } from 'vuex'
 
 export default {
   name: 'UserLikes',
@@ -30,15 +36,28 @@ export default {
   data () {
     return {
       userLikes: [],
-      currentUser: [],
-      userId: ''
+      userId: '',
+      initialUser: [],
+      initialFollowers: [],
+      initialFollowing: false
     }
+  },
+  computed: {
+    ...mapState(['currentUser'])
+  },
+  beforeRouteUpdate (to, from, next) {
+    const { id } = to.params
+    this.fetchUserLikes(id)
+    this.fetchUser(id)
+    this.fetUserFollowers(id)
+    next()
   },
   created () {
     const { id } = this.$route.params
     this.userId = id
     this.fetchUserLikes(id)
-    this.fetchCurrentUser()
+    this.fetchUser(id)
+    this.fetUserFollowers(id)
   },
   methods: {
     async fetchUserLikes (userId) {
@@ -54,15 +73,48 @@ export default {
         })
       }
     },
-    async fetchCurrentUser () {
+    // async fetchCurrentUser () {
+    //   try {
+    //     const { data } = await userAPI.getCurrentUser()
+    //     this.currentUser = data
+    //   } catch (e) {
+    //     console.log(e)
+    //     Toast.fire({
+    //       icon: 'error',
+    //       title: '讀取現在使用者資料失敗'
+    //     })
+    //   }
+    // },
+    // 取得目前路由的使用者資料
+    async fetchUser (userId) {
       try {
-        const { data } = await userAPI.getCurrentUser()
-        this.currentUser = data
+        const { data } = await userAPI.getUser({ userId })
+        this.initialUser = data
       } catch (e) {
         console.log(e)
         Toast.fire({
           icon: 'error',
-          title: '讀取現在使用者資料失敗'
+          title: 'user頁面資料讀取失敗'
+        })
+      }
+    },
+    // 取得目前路由的使用者的followers清單，和currentUser比對，如果currentUser在清單中就是following狀態
+    async fetUserFollowers (userId) {
+      try {
+        const { data } = await userAPI.getUserFollowers({ userId })
+        this.initialFollowers = data
+        this.initialFollowers.forEach(item => {
+          if (item.followerId !== this.currentUser.id) {
+            this.initialFollowing = false
+          } else {
+            this.initialFollowing = true
+          }
+        })
+      } catch (e) {
+        console.log(e)
+        Toast.fire({
+          icon: 'error',
+          title: '讀取不到跟隨者的資料'
         })
       }
     }
