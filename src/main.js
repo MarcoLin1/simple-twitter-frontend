a3aae55bc53295a701be17cba0d9b4d580eec415
Marco Lin <pohanlin0605@gmail.com>
2021-07-17 01:39:48 +0800

    install vue socket

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import App from './App.vue'
 import router from './router'
 import './assets/scss/reset.scss'
 import store from './store'
+import VueSocketIo from 'vue-socket.io'
+import SocketIo from 'socket.io-client'
+
+Vue.use(new VueSocketIo({
+  debug: true,
+  connection: SocketIo('infinite-mountain-11239.herokuapp.com/socket.io/?EIO=4&transport=websocket', { transports: ['websocket'] }),
+  vuex: {
+    store,
+    actionPrefix: 'SOCKET_',
+    mutationPrefix: 'SOCKET_'
+  },
+  options: { path: '' }
+}))
 
 Vue.config.productionTip = false
 
