a7ff75ae5b642764e9aada89ae25866c0a88b801
Marco Lin <pohanlin0605@gmail.com>
2021-07-26 10:25:56 +0800

    feat: add subscribe API

---
+++ src/apis/subscribe.js
@@ -0,0 +1,13 @@
+import { apiHelper } from '../utils/helper'
+
+export default {
+  add ({ recipientId, subscriberId }) {
+    return apiHelper.post('/subscriptions', { recipientId, subscriberId })
+  },
+  cancel ({ recipientId, subscriberId }) {
+    return apiHelper.delete(`/subscriptions/${recipientId}`, { data: { subscriberId } })
+  },
+  history ({ id }) {
+    return apiHelper.get(`/notifications/${id}`)
+  }
+}
